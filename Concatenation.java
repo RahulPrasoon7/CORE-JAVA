feb3641932bb79c64e708ed84e806bd5a05f3af9
RahulPrasoon7 <singh32903@gmail.com>
2026-02-13 13:41:19 +0530

    Add files via upload

---
+++ Concatenation.java
@@ -0,0 +1,12 @@
+
+public class Concatenation {
+	public static void main(String[] args) {
+		System.out.println("RAHUL"+2+2);
+		System.out.println("rahul"+(2+2));
+		System.out.println(2+2+"Rahul");
+		System.out.println("rahul"+5*8);
+		System.out.println("rahul"+-8+7);
+		System.out.println("rahul"+(-5+8));
+	}
+
+}
