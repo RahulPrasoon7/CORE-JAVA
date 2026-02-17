17bf829172380dbd35c65f70b3da751363a04750
RahulPrasoon7 <singh32903@gmail.com>
2026-02-17 13:16:12 +0530

    Add files via upload

---
+++ Vehicle.java
@@ -0,0 +1,12 @@
+package com.tka;
+
+public class Vehicle {
+	public static void main(String[] args) {
+		System.out.println("Model Name:"+"Mercedes-Maybach GLS 600 ");
+		System.out.println("Engine:"+"4.0-liter V8 biturbo");
+		System.out.println("Assembly:"+"Vance,Alabama");
+		System.out.println("Price:"+"4.10Cr");
+		System.out.println("Fuel Type:"+"Petrol");
+	}
+
+}
