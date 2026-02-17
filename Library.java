7ed24bd2368d4089525dcf6ba6ee820328a5589a
RahulPrasoon7 <singh32903@gmail.com>
2026-02-17 13:15:25 +0530

    Add files via upload

---
+++ Library.java
@@ -0,0 +1,15 @@
+package com.tka;
+
+public class Library {
+	public static void main(String[] args) {
+		System.out.println("Library Name:"+"Peterson Library Rajguru");
+		System.out.println("Libray type:"+"Paid");
+		System.out.println("Card Holder:"+"Rahul Kumar Parsoon");
+		System.out.println("Timimg"+"10.am  ,9.pm");
+		System.out.println("Facility Provided:"+"Wifi,Water");
+		System.out.println("Book Alloted:"+"only 5 days");
+		System.out.println("Submission Date:"+"Within 5 days ");
+		System.out.println("if you don't submit the book you should pay the penalty it is compulsary!");
+	}
+
+}
