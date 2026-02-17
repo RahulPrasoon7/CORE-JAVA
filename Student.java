1d61ad3c5ed6a44bccb8cce51a28aa9d53b195bb
RahulPrasoon7 <singh32903@gmail.com>
2026-02-17 13:17:00 +0530

    Add files via upload

---
+++ Student.java
@@ -0,0 +1,14 @@
+package com.tka;
+
+public class Student {
+	public static void main(String[] args) {
+		System.out.println("Name:"+"Rahul Kumar Prasoon");
+		System.out.println("class:"+"Ty BSC.CS");
+		System.out.println("10th per:"+ 65);
+		System.out.println("12th per :"+62);
+		System.out.println("1yr:"+"8.86 CGPA");
+		System.out.println("2yr:"+"8.26 CGPA");
+		
+	}
+
+}
