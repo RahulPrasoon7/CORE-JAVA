6c056c0328f7dc3c61a39500a5e1071c65257c7e
RahulPrasoon7 <singh32903@gmail.com>
2026-02-17 13:11:57 +0530

    Add files via upload

---
+++ Bank.java
@@ -0,0 +1,15 @@
+package com.tka;
+
+public class Bank {
+	public static void main(String[] args) {
+		System.out.println("Account Holder Name:"+"Rahul Kumar Prasoon");
+		System.out.println("Account Number:"+4578587448957l);
+		System.out.println("IFSC Code:"+"ubin7878585");
+		System.out.println("Account Type:"+"Saving Account");
+		System.out.println("D.O.B:"+"29-July-2005");
+		System.out.println("Branch Name:"+"Bhosari");
+		System.out.println("Bank Name:"+"Union Bank Of Indian");
+		
+	}
+
+}
