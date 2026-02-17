d0b8b9f7589c39899cbccca115b83b81f2c7f370
RahulPrasoon7 <singh32903@gmail.com>
2026-02-17 13:13:44 +0530

    Add files via upload

---
+++ Hospital.java
@@ -0,0 +1,14 @@
+package com.tka;
+
+public class Hospital {
+	public static void main(String[] args) {
+		System.out.println("Hospital Name:"+"YCM");
+		System.out.println("Patient Name:"+"Ganesh Kokare");
+		System.out.println("Patient Address:"+"Bhosari");
+		System.out.println("Diseases:"+"Viral Fever");
+		System.out.println("Blood Group:"+"B+");
+		System.out.println("Patient Age:"+23);
+		System.out.println("Doctor Name:"+"Dr.Rahul Prasoon");
+	}
+
+}
