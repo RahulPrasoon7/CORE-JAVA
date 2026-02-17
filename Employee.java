7574908d1444c0830505290640c9b828dcf8b375
RahulPrasoon7 <singh32903@gmail.com>
2026-02-17 19:14:26 +0530

    Add files via upload

--- Employee.java
+++ Employee.java
@@ -2,12 +2,22 @@ package com.tka;
 
 public class Employee {
 public static void main(String[] args) {
-	System.out.println("EmpId:"+102);
-	System.out.println("Emp Name:"+"Rahul Kumar Prasoon");
-	System.out.println("Emp Age:"+25);
-	System.out.println("Designation:"+"Software Developer");
-	System.out.println("Employee Department:"+"IT");
-	System.out.println("salary:"+800000);
-	System.out.println("Experience:"+"5yrs");
+	
+	String EmpName="Rahul Kumar Prasoon ";
+	byte EmpID=102;
+	byte Age=21;
+	String Designation="Software Developer";
+	String EmpDepartment="IT";
+	double Salary=8000000;
+	float Experience=4.5f;
+	
+	
+	System.out.println("EmpId:"+EmpID);
+	System.out.println("Emp Name:"+EmpName);
+	System.out.println("Emp Age:"+Age);
+	System.out.println("Designation:"+Designation);
+	System.out.println("Employee Department:"+EmpDepartment);
+	System.out.println("salary:"+Salary);
+	System.out.println("Experience:"+Experience);
 }
 }
