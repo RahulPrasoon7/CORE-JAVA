b4a5785cce4a4ebc7941cd3c4b7c169968228712
RahulPrasoon7 <singh32903@gmail.com>
2026-02-17 13:12:51 +0530

    Add files via upload

---
+++ Employee.java
@@ -0,0 +1,13 @@
+package com.tka;
+
+public class Employee {
+public static void main(String[] args) {
+	System.out.println("EmpId:"+102);
+	System.out.println("Emp Name:"+"Rahul Kumar Prasoon");
+	System.out.println("Emp Age:"+25);
+	System.out.println("Designation:"+"Software Developer");
+	System.out.println("Employee Department:"+"IT");
+	System.out.println("salary:"+800000);
+	System.out.println("Experience:"+"5yrs");
+}
+}
