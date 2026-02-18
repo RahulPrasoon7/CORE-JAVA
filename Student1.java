cf710989ae6c4b3c3800f8a5f391d4105526b8b3
RahulPrasoon7 <singh32903@gmail.com>
2026-02-18 10:10:02 +0530

    Add files via upload

---
+++ Student1.java
@@ -0,0 +1,25 @@
+package com.tka;
+
+public class Student1 {
+	public static void main(String[] args) {
+		
+		String Name="Rahul Kumar Prasoon";
+		String Class="Ty Bsc.cs";
+		float HighSchoolMarks=65.0f;
+		float IntermediateMarks=62.50f;
+		float firstYrs =8.86f;
+		float secondYrs=8.26f;
+		
+		
+		
+		
+		System.out.println("Name:"+Name);
+		System.out.println("class:"+Class);
+		System.out.println("10th per:"+ HighSchoolMarks);
+		System.out.println("12th per :"+IntermediateMarks);
+		System.out.println("1yr:"+firstYrs);
+		System.out.println("2yr:"+secondYrs);
+		
+	}
+
+}
