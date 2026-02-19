9bc71b17229b64863bbaaffaed8782fb8f51e913
Rahul Kumar Prasoon <singh32903@gmail.com>
2026-02-19 11:55:35 +0530

    Add files via upload

---
+++ SizeBitDatatype.java
@@ -0,0 +1,15 @@
+package com.dataType;
+
+public class SizeBitDatatype {
+	public static void main(String[] args) {
+		System.out.println("Byte Size is " + Byte.SIZE + "  Bit");
+		System.out.println("Short Size is " + Short.SIZE + " Bit");
+		System.out.println("Integer Size is " + Integer.SIZE + " Bit");
+		System.out.println("Long Size is " + Long.SIZE + " Bit");
+		System.out.println("Float Size is " + Float.SIZE + " Bit");
+		System.out.println("Double Size is " + Double.SIZE + " Bit");
+		System.out.println("Character Size is " + Character.SIZE + " Bit");
+		System.out.println("Boolean is not defined!!!");
+	}
+
+}
