dd3257868388005876fbe1b003f420a24af2cd78
Rahul Kumar Prasoon <singh32903@gmail.com>
2026-02-24 17:30:42 +0530

    Add files via upload

--- Bank.java
+++ Bank.java
@@ -1,20 +1,24 @@
-package com.tka;
+package com.variables;
 
 public class Bank {
 	public static void main(String[] args) {
-		
-		String Name="Rahul Kumar Prasoon";
-		long Account Number=47589657979565l;
-		String IFSC Code="ubin7878585";
-		
-		System.out.println("Account Holder Name:"+String);
-		System.out.println("Account Number:"+4578587448957l);
-		System.out.println("IFSC Code:"+"ubin7878585");
-		System.out.println("Account Type:"+"Saving Account");
-		System.out.println("D.O.B:"+"29-July-2005");
-		System.out.println("Branch Name:"+"Bhosari");
-		System.out.println("Bank Name:"+"Union Bank Of Indian");
-		
+
+		String name = "Rahul Kumar Prasoon";
+		long accountNumber = 47589657979565l;
+		String IFSCCode = "ubin7878585";
+		String accountType = "Saving Account";
+		String dob = "29-July-2005";
+		String branchName = "Bhosari";
+		String bankName = "Union Bank of India ";
+
+		System.out.println("Account Holder Name:" + name);
+		System.out.println("Account Number:" + accountNumber);
+		System.out.println("IFSC Code:" + IFSCCode);
+		System.out.println("Account Type:" + accountType);
+		System.out.println("D.O.B:" + dob);
+		System.out.println("Branch Name:" + branchName);
+		System.out.println("Bank Name:" + bankName);
+
 	}
 
 }
