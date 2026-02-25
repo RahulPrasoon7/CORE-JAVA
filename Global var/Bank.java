2601ae5df276aa51529f95f81147a561a7122e13
Rahul Kumar Prasoon <singh32903@gmail.com>
2026-02-25 12:31:15 +0530

    Add files via upload

--- Global var/Bank.java
+++ Global var/Bank.java
@@ -1,10 +1,26 @@
 package global;
 
 public class Bank {
-	
-	
-	
-	
-	
+	static String name = "Rahul Kumar Prasoon";
+	static long accountNumber = 47589657979565l;
+	static String IFSCCode = "ubin7878585";
+	static String accountType = "Saving Account";
+	static String dob = "29-July-2005";
+	static String branchName = "Bhosari";
+	static String bankName = "Union Bank Of India";
+	static String disability;
+
+	public static void main(String[] args) {
+
+		System.out.println(name);
+		System.out.println(accountNumber);
+		System.out.println(IFSCCode);
+		System.out.println(accountType);
+		System.out.println(dob);
+		System.out.println(branchName);
+		System.out.println(bankName);
+		System.out.println(disability);
+
+	}
+
 }
-	
