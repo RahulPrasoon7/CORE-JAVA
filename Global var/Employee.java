76381a848d2da12ca25e26ae8ad70d68fa16b84a
Rahul Kumar Prasoon <singh32903@gmail.com>
2026-02-25 12:19:07 +0530

    Add files via upload

---
+++ Global var/Employee.java
@@ -0,0 +1,22 @@
+package global;
+
+public class Employee {
+	static String EmpName = "Rahul Kumar Prasoon ";
+	static byte EmpID = 102;
+	static byte Age = 21;
+	static String Designation = "Software Developer";
+	static String EmpDepartment = "IT";
+	static double Salary = 8000000;
+	static float Experience;
+
+	public static void main(String[] args) {
+
+		System.out.println(EmpID);
+		System.out.println(EmpName);
+		System.out.println(Age);
+		System.out.println(Designation);
+		System.out.println(EmpDepartment);
+		System.out.println(Salary);
+		System.out.println(Experience);
+	}
+}
