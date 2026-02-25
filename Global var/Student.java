8154ec6129f77eb805ced21b9278e975aac86b65
Rahul Kumar Prasoon <singh32903@gmail.com>
2026-02-25 12:19:50 +0530

    Add files via upload

---
+++ Global var/Student.java
@@ -0,0 +1,23 @@
+package global;
+
+public class Student {
+	static String Name = "Rahul Kumar Prasoon";
+	static String Class = "Ty Bsc.cs";
+	static float HighSchoolMarks = 65.0f;
+	static float IntermediateMarks = 62.50f;
+	static float firstYrs = 8.86f;
+	static float secondYrs = 8.26f;
+	static String Disability;
+
+	public static void main(String[] args) {
+		System.out.println(Name);
+		System.out.println(Class);
+		System.out.println(HighSchoolMarks);
+		System.out.println(IntermediateMarks);
+		System.out.println(firstYrs);
+		System.out.println(secondYrs);
+		System.out.println(Disability);
+
+	}
+
+}
