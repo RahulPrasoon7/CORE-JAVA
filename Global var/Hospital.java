662edc5d54a01520e564b7afce92ab4291020e5b
Rahul Kumar Prasoon <singh32903@gmail.com>
2026-02-25 12:19:29 +0530

    Add files via upload

---
+++ Global var/Hospital.java
@@ -0,0 +1,22 @@
+package global;
+
+public class Hospital {
+	static int hospitalId = 21;
+	static String hName = "YCM";
+	static String patientName = "Ganesh Kokare";
+	static String bloodTestReport;
+	static int weight = 47;
+	static float height = 5.5f;
+
+	public static void main(String[] args) {
+
+		System.out.println(hospitalId);
+		System.out.println(hName);
+		System.out.println(patientName);
+		System.out.println(bloodTestReport);
+		System.out.println(weight);
+		System.out.println(height);
+
+	}
+
+}
