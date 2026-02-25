7d2cd7415b8adb675acc8eed4b06ef2e4027b3f4
Rahul Kumar Prasoon <singh32903@gmail.com>
2026-02-25 12:15:24 +0530

    Add files via upload

---
+++ variables/Employee.java
@@ -0,0 +1,27 @@
+package com.variables;
+
+public class Employee {
+	public static void main(String[] args) {
+		String empName="Rahul Prasoon";
+		int empId=102;
+		String department="IT";
+		String designation="Software Developer";
+		double salary=75000;
+		String officeTiming="10:00 to 5:00";
+		String experience="5yrs";
+		
+		System.out.println("Employee Name="+empName);
+		System.out.println("Employee ID="+empId);
+		System.out.println("Employee Designation="+designation);
+		System.out.println("Employee Department="+department);
+		System.out.println("Employee Salary="+salary);
+		System.out.println("Employee office time= "+officeTiming);
+		System.out.println("Employee Experience= "+experience);
+		
+		
+		
+		
+		
+	}
+
+}
