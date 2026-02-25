6259b17816add2e5bcdcd97ec8921494d1ef35d4
Rahul Kumar Prasoon <singh32903@gmail.com>
2026-02-25 12:15:47 +0530

    Add files via upload

---
+++ variables/Hospital.java
@@ -0,0 +1,26 @@
+package com.variables;
+
+public class Hospital {
+		public static void main(String[] args) {
+			
+			String hospitalName="YCM";
+			String patientName="Ganesh Kokare";
+			String address="Bhosari";
+			String disease="Viral Fever";
+			String bloodGroup="B+";
+			int age=23;
+			String doctorName="Dr. Rahul Prasoon";
+			
+			System.out.println("Hospital Name:"+hospitalName);
+			System.out.println("Patient Name:"+patientName);
+			System.out.println("Patient Address:"+address);
+			System.out.println("Diseases:"+disease);
+			System.out.println("Blood Group:"+bloodGroup);
+			System.out.println("Patient Age:"+age);
+			System.out.println("Doctor Name:"+doctorName);
+		}
+
+	}
+
+
+
