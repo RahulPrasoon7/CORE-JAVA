c92d157077cb8e0304f7e8e3bb21cf01a3f31f5d
Rahul Kumar Prasoon <singh32903@gmail.com>
2026-02-25 12:13:27 +0530

    Add files via upload

---
+++ Datatype/SizeDataType.java
@@ -0,0 +1,16 @@
+package com.dataType;
+
+public class SizeDataType {
+	public static void main(String[] args) {
+		System.out.println("Byte size is " + Byte.BYTES + " Bytes");
+
+		System.out.println("Byte size is " + Byte.BYTES + " Bytes");
+		System.out.println("Short size is " + Short.BYTES + " Bytes");
+		System.out.println("Integer size is " + Integer.BYTES + " Bytes");
+		System.out.println("Long size is " + Long.BYTES + " Bytes");
+		System.out.println("Double size is " + Double.BYTES + " Bytes");
+		System.out.println("Float size is " + Float.BYTES + " Bytes");
+
+		System.out.println("Character size is " + Character.BYTES + " Bytes");
+	}
+}
