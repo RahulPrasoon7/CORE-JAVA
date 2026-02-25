1080f4329a86b1b90bdf3144d52aa466ace91bdb
Rahul Kumar Prasoon <singh32903@gmail.com>
2026-02-25 12:12:03 +0530

    Add files via upload

---
+++ Datatype/DataTypesSize.java
@@ -0,0 +1,27 @@
+package com.dataType;
+
+public class DataTypesSize {
+public static void main(String[] args) {
+	byte age=21;
+	short seatNumber=12452;
+			int pincode=759658785;
+			long MobileNumber=78757857875l;
+			
+			float height=7.9f;
+			double salary=85.897989;
+			
+			char gmail='@';
+			boolean checkPassed=true;
+			
+			System.out.println("StudentAge ="+ age);
+			System.out.println("Student seatNumber="+seatNumber);
+			System.out.println("Student Pincode="+pincode);
+			System.out.println("Student MobileNumber="+MobileNumber);
+			System.out.println("Student Height="+height);
+			System.out.println("Student Salary="+salary);
+			System.out.println("Student gmail="+gmail);
+			System.out.println("Student Status"+checkPassed);
+			
+	
+}
+}
