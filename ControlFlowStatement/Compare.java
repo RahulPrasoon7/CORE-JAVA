4874be532c94e1d47dae4531e8dbb60ac055245b
Rahul Kumar Prasoon <singh32903@gmail.com>
2026-03-06 11:08:17 +0530

    Add files via upload

---
+++ ControlFlowStatement/Compare.java
@@ -0,0 +1,15 @@
+package com.controlflow;
+
+public class Compare {
+	public static void main(String[] args) {
+		int a = 21;
+		int b = 10;
+		if (a > b) {
+			System.out.println(a + "Number is Greater..!");
+		} else {
+			System.out.println(b + "Number is Greater...!");
+		}
+
+	}
+
+}
