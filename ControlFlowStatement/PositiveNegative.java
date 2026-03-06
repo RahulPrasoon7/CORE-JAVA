0cfe4de457d2f030cc38a0a1dbd4ec08c2d32428
Rahul Kumar Prasoon <singh32903@gmail.com>
2026-03-06 11:07:51 +0530

    Add files via upload

---
+++ ControlFlowStatement/PositiveNegative.java
@@ -0,0 +1,13 @@
+package com.controlflow;
+
+public class PositiveNegative {
+	public static void main(String[] args) {
+		int a=25;
+		if(a>0) {
+			System.out.println("Number is positive ...!");
+		}else {
+			System.out.println("Number is Negative... !");
+		}
+	}
+
+}
