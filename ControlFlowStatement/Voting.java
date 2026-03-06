d48d19207f36e3034ba019090d0698f8257a88aa
Rahul Kumar Prasoon <singh32903@gmail.com>
2026-03-06 11:08:41 +0530

    Add files via upload

---
+++ ControlFlowStatement/Voting.java
@@ -0,0 +1,13 @@
+package com.controlflow;
+
+public class Voting {
+	public static void main(String[] args) {
+		int Age = 18;
+		if (Age >= 18) {
+			System.out.println("Person is Eligible for voting... !");
+		} else {
+			System.out.println("Is Not Eligible... !");
+		}
+	}
+
+}
