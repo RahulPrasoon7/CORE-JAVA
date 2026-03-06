1a6503743a596afcf150705918a36afdefc7065d
Rahul Kumar Prasoon <singh32903@gmail.com>
2026-03-06 11:07:31 +0530

    Add files via upload

---
+++ ControlFlowStatement/EvenOdd.java
@@ -0,0 +1,13 @@
+package com.controlflow;
+
+public class EvenOdd {
+	public static void main(String[] args) {
+		int age=24;
+				if(age%2==0) {
+					System.out.println("It is Even Number");
+				}else {
+					System.out.println("Otherwise it is Odd");
+				}
+	}
+
+}
