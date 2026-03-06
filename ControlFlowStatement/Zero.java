a30e17b04b66eb4192db20da929720b1b6433533
Rahul Kumar Prasoon <singh32903@gmail.com>
2026-03-06 11:09:05 +0530

    Add files via upload

---
+++ ControlFlowStatement/Zero.java
@@ -0,0 +1,17 @@
+package com.controlflow;
+
+public class Zero {
+	public static void main(String[] args) {
+		int a = 0;
+		if (a > 0) {
+			System.out.println("Number is positive");
+		} else if (a < 0) {
+			System.out.println("Number is negative");
+
+		} else {
+			System.out.println("Number is zero");
+		}
+
+	}
+
+}
