800a11b46e969726eb771f801f8baadccc647f05
Rahul Kumar Prasoon <singh32903@gmail.com>
2026-03-07 23:19:43 +0530

    Add files via upload

---
+++ ControlFlowStatement/Divisible.java
@@ -0,0 +1,13 @@
+package com.controlflow;
+
+public class Divisible {
+	public static void main(String[] args) {
+		int a = 25;
+		if (a % 5 == 0) {
+			System.out.println(a + "Number id divisible by 5:");
+		} else {
+			System.out.println("Number is not divisible by 5");
+		}
+	}
+
+}
