4aec591e167615d6139ba1e3a6c4ce1af40dc0d7
Rahul Kumar Prasoon <singh32903@gmail.com>
2026-03-19 14:36:07 +0530

    Add files via upload

---
+++ break&Continue/EndingContinue.java
@@ -0,0 +1,12 @@
+
+public class EndingContinue {
+	public static void main(String[] args) {
+		for (int i = 1; i <= 100; i++) {
+			if (i % 10 == 5) {
+				continue;
+			}
+			System.out.println(i);
+		}
+	}
+
+}
