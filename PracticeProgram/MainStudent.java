e90eaf6eaf54481a3b9799740be0bfc8d1bcf273
Rahul Kumar Prasoon <singh32903@gmail.com>
2026-03-19 18:53:09 +0530

    Add files via upload

---
+++ PracticeProgram/MainStudent.java
@@ -0,0 +1,9 @@
+
+public class MainStudent {
+	public static void main(String[] args) {
+		Student s1 = new Student();
+		System.out.println("Student Id:" + (s1.id = 102));
+		System.out.println("Name of the Student:" + (s1.name = "Rahul"));
+	}
+
+}
