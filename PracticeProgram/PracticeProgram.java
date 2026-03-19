a01607f00742ef5cccd1cd4b486d5c8e3c1d6672
Rahul Kumar Prasoon <singh32903@gmail.com>
2026-03-19 18:49:40 +0530

    Add files via upload

---
+++ PracticeProgram/PracticeProgram.java
@@ -0,0 +1,61 @@
+
+public class PracticeProgram {
+	public static void main(String[] args) {
+//		int rows = 5;
+//		for (int i = 0; i < rows; i++) {
+//			for (int j = 0; j <= i; j++) {
+//				System.out.print((char) ('A' + j));
+//			}
+//			System.out.println();
+//		}
+
+//		int rows = 5;
+//		for (int i = 0; i < rows; i++) {
+//			for (int j = 0; j <= i; j++) {
+//				System.out.print("*");
+//			}
+//			System.out.println();
+//		}
+//		for (int i = rows - 1; i >= 1; i--) {
+//			for (int j = 1; j <= i; j++) {
+//				System.out.print("*");
+//			}
+//			System.out.println();
+//		}
+//		int Language = 5;
+//		switch (Language) {
+//		case 1:
+//			System.out.println("Java");
+//			break;
+//		case 2:
+//			System.out.println("Python");
+//			break;
+//		case 3:
+//			System.out.println("C++");
+//			break;
+//		case 4:
+//			System.out.println("JavaScript");
+//			break;
+//		case 5:
+//			System.out.println("Sql");
+//			break;
+//		default:
+//			System.out.println("Invalid Input");
+//		}
+
+//		double radius = 5;
+//		double circleArea = Math.PI * radius * radius;
+//		System.out.println("Area of Circle :" + circleArea);
+
+//		double side = 4;
+//		double squareArea = side * side;
+//		System.out.println("Area od Square is :" + squareArea);
+
+//		double length = 6;
+//		double breadth = 8;
+//		double rectangleArea = length * breadth;
+//		System.out.println("Area of Rectangle:" + rectangleArea);
+
+	}
+
+}
