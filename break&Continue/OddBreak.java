607cf0ba5c8df097f42c4c14fd488e42d55941ad
Rahul Kumar Prasoon <singh32903@gmail.com>
2026-03-19 14:36:56 +0530

    Add files via upload

---
+++ break&Continue/OddBreak.java
@@ -0,0 +1,13 @@
+
+public class OddBreak {
+	public static void main(String[] args) {
+		int i=1;
+		for(i=1;i<=100;i++) {
+			if(i%2!=0) {
+				break;
+			}
+			System.out.println(i);
+		}
+	}
+
+}
