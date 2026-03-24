ff6df0f49a0451e1744435057495381a97c6bcf6
Rahul Kumar Prasoon <singh32903@gmail.com>
2026-03-24 10:58:50 +0530

    Add files via upload

---
+++ Method/MainMethod.java
@@ -0,0 +1,33 @@
+package com.tka;
+
+public class MainMethod {
+	public static void main(String[] args) {
+		Method m =new Method() ;
+//		m.checkFibonacci(7);
+//m.checkEvenOdd();
+//		m.checkReverse();
+//			m.checkPalindrome();
+//		m.checkPattern();
+//		m.name="Rahul Prasoon";
+//		m.age=21;
+//		m.Height=5.8f;
+//		m.aadharNo=78785744796575555l;
+//		m.marks10th="62%";
+//		m.marks12th="63%";
+//		m.marks1styrs="8.7 Sgpa";
+//		m.marks2ndyrs="8.3 Sgpa";
+//		m.mobileNo=7575848797895l;
+//		m.acchivement="Winner of 2 time In Blind bee Competition";
+//		m.Hobbies="Watching Series";
+//		m.Languages="Hindi ,English,Marathi";
+//		m.panNo="rahu624272ra";
+//		m.programmingLanguages="c,c++,java,python,html,css,php,javaScript";
+//		m.Stream="Computer Science";
+//		m.displaymyInfo();
+	
+//	m.Addtion();
+//		m.Square();
+		m.LargestNumber();
+	}
+
+}
---
+++ Method/Method.java
@@ -0,0 +1,117 @@
+package com.tka;
+
+public class Method {
+//	Check Fibonacci Series
+//	public void checkFibonacci(int n) {
+//		int a=0,b=1;
+//		System.out.println(a+" "+b+" ");
+//		for(int i=2;i<n;i++) {
+//			int c=a+b;
+//			System.out.println(c+" ");
+//			a=b;
+//			b=c;
+//		}
+//	Check Even Odd
+//		public void checkEvenOdd() {
+//			int a=25;
+//			if(a%2==0) {
+//				System.out.println(a+" Number is Even");
+//			}else {
+//				System.out.println(a+"Number is Odd");
+//			}
+// Check Reverse
+//	public void checkReverse() {
+//	int num=1475,reverse=0;
+//	while(num!=0) {
+//		int digit=num%10;
+//		reverse=reverse*10+digit;
+//		num=num/10;
+//		
+//	}System.out.println(reverse);
+//	
+//	}
+// check Palindrome
+//	public void checkPalindrome() {
+//		int num = 123, reverse = 0, original;
+//		original = num;
+//		while (num != 0) {
+//			int digit = num % 10;
+//			reverse = reverse * 10 + digit;
+//			num = num / 10;
+//		}
+//		if (original == reverse) {
+//			System.out.println("It is Palindrome Number" + reverse);
+//		} else {
+//			System.out.println("It is not a Palindrome Number" + reverse);
+//		}
+//	}
+	// Check Pattern
+//	public void checkPattern() {
+//		for(int i=1;i<=5;i++) {
+//			for(int j=1;j<=i;j++) {
+//				System.out.print("*");
+//			}
+//			System.out.println();
+//		}
+//	}
+//	Check myInfo
+
+//	String name;
+//	int age;
+//	float Height;
+//	String Stream;
+//	String marks10th;
+//	String marks12th;
+//	String marks1styrs;
+//	String marks2ndyrs;
+//	String Hobbies;
+//	long mobileNo;
+//	long aadharNo;
+//	String panNo;
+//	String acchivement;
+//	String Languages;
+//	String programmingLanguages;
+//
+//	public void displaymyInfo() {
+//		System.out.println("Name="+name);
+//		System.out.println("Age="+age);
+//		System.out.println("Height="+Height);
+//		System.out.println("Stream="+Stream);
+//		System.out.println("Marks10th="+marks10th);
+//		System.out.println("Marks12th="+marks12th);
+//		System.out.println("Marks 1styr="+marks1styrs);
+//		System.out.println("Marks 2ndyr="+marks2ndyrs);
+//		System.out.println("Hobbies="+Hobbies);
+//		System.out.println("Mobile No.="+mobileNo);
+//		System.out.println("Aadhar No.="+aadharNo);
+//		System.out.println("Pan No.="+panNo);
+//		System.out.println("Acchivement="+acchivement);
+//		System.out.println("Languages="+Languages);
+//		System.out.println("Progrraming Languages="+programmingLanguages);
+//		 
+//		
+//		
+//		
+//	}
+//	}
+//public void Addtion( ) {
+//	int num1=57;
+//	int num2=54;
+//int 	result=num1+num2;
+//System.out.println(result);
+//}
+//	public void Square() {
+//		int a=5;
+//		int result=a*a;
+//		System.out.println(result);
+//	}
+	public void LargestNumber() {
+		int a = 75;
+		int b = 45;
+		if (a > b) {
+			System.out.println(a + "The Number is largest");
+		} else {
+			System.out.println(b + "the number is largest");
+		}
+	}
+}
