c4907f77a6c90f8a98c58443ad8f4c15991ab45e
Rahul Kumar Prasoon <singh32903@gmail.com>
2026-03-28 12:59:24 +0530

    Add files via upload

---
+++ Prime.java
@@ -0,0 +1,19 @@
+
+public class Prime {
+	public static void main(String[] args) {
+		int num=6;
+		int count=0;
+		for(int i=1;i<=num;i++) {
+			if(num%i==0) {
+				count ++;
+			}
+		}
+		if(count==2) {
+			System.out.println("The Number is prime");
+		}else {
+			System.out.println("The number is not prime");
+		}
+			
+	}
+
+}
