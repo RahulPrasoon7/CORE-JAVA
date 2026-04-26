1b88b316ba4255c9d3b0f3173707102c596b1096
Rahul Kumar Prasoon <singh32903@gmail.com>
2026-04-26 18:44:05 +0530

    Add files via upload

---
+++ CheckVowels.java
@@ -0,0 +1,14 @@
+package com.practice;
+
+public class CheckVowels {
+	public static void main(String[] args) {
+		char ch = 'I';
+		if (ch == 'A' || ch == 'E' || ch == 'I' || ch == 'O' || ch == 'U') {
+			System.out.println("The Alphabet is Vowels");
+
+		} else {
+			System.out.println("The Alphabet is Constant ");
+		}
+	}
+
+}
