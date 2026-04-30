113c8fbc07c92249e40fbc449facade4fbec428b
Rahul Kumar Prasoon <singh32903@gmail.com>
2026-04-30 21:01:34 +0530

    Add files via upload

---
+++ ProgramPractice.java
@@ -0,0 +1,26 @@
+package com.tka;
+
+public class ProgramPractice {
+	public static void main(String[] args) {
+		int[][] num= {{25,28,96},{54,25,85},{52,25,85}};
+//		int sum=0;
+//		for (int i = 0; i < num.length; i++) {
+//            for (int j = 0; j < num[i].length; j++) {
+//                sum = sum + num[i][j];
+//            }
+//		}
+//		System.out.println(sum);
+//		 for (int i = 0; i < num.length; i++) {
+//	            for (int j = 0; j < num[i].length; j++) {
+//
+//	                if (num[i][j] % 2 == 0) {
+//	                    System.out.println(num[i][j] + " is Even");
+//	                } else {
+//	                    System.out.println(num[i][j] + " is Odd");
+//	                }
+//	            }
+//	}
+//
+		
+}
+}
